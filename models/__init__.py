ea74363c8d3bbd6706c6c4cc03f9a74683d8b693
kyle sadler <kylesadler5@gmail.com>
2020-12-02 23:22:30 -0600

    removed files

--- models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .survival_net import SurvivalNet
-from .unet3d import Unet3D
-from .resnet1d import ResNet1D
--- models/resnet1d.py
+++
@@ -1,9 +0,0 @@
-from .Multi_Scale_1D_ResNet.multi_scale_ori import MSResNet as ResNet1D
-import torch
-
-# class ResNet1D(torch.nn.Module):
-#     def __init__(self):
-#         super().__init__()
-
-#     def forward(self, x):
-#         return x
