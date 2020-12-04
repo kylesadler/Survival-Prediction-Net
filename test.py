6afb18b231580f3d6500080b54fb5078b221d1af
kyle sadler <kylesadler5@gmail.com>
2020-12-04 08:33:06 -0600

    moved code around

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import torch
-from models import SurvivalNet
+from models import Unet3D
 
 from data_sampler import (get_train_dataflow, get_eval_dataflow, get_test_dataflow)
 from eval import (eval_brats, pred_brats, segment_one_image, segment_one_image_dynamic)
@@ -14,15 +14,13 @@ except:
     pass
 
 x = torch.randn(2, 5, 50, 50, 50)
-age = torch.randn(2, 1, 512) # must be 512
 y = torch.randn(2, 5)
 
-model = SurvivalNet(brain_input_channels=5)
+model = Unet3D(5)
 
 try:
     x = x.cuda()
     y = y.cuda()
-    age = age.cuda()
     model.cuda()
 except:
     pass
@@ -32,7 +30,7 @@ criterion = torch.nn.MSELoss(reduction='sum')
 optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
 for t in range(1500):
     # Forward pass: Compute predicted y by passing x to the model
-    y_pred = model(x, age)
+    y_pred = model(x)
 
     # Compute and print loss
     loss = criterion(y_pred, y)
